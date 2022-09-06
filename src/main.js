bcba2f42de8fc01b1c0cc2c3415bad6c7b9234e9
Tarık Sefa <by.krawhat@gmail.com>
2022-09-06 20:31:49 +0300

    Add usage(as a comment) of multi language plugin

--- src/main.js
+++ src/main.js
@@ -5,6 +5,28 @@ import router from './router'
 import store from './store'
 
 const app = createApp(App)
+
+/*
+    Custom multi language plugin
+    - Language texts can be changed / added on the Store.
+    - Usage: $t('text')
+    - You can navigate in language object(EN/TR) with '.' : 
+
+        result: {
+            title: 'Hello WORLD',
+            description: {
+                title: 'Gosmac',
+                text: 'I love programming.'
+            }
+        } 
+
+        $t('resut.title') => 'Hello WORLD'
+        $t('result.description.title') => 'Gosmac'
+        $t('result.description.text') => 'I love programming.'
+        $t('result.gosmac') => null
+
+
+*/
 app.config.globalProperties.$t = store.getters.getText
 
 app.use(store)
